7279afc91d0148a7e4e1bbe20350be4bebae7fe8
mohit khanna <mohit.personal1987@gmail.com>
2019-03-12 16:59:09 +0530

    build of app

---
+++ app/build/precache-manifest.b42837e28db57d73413a5f6a5f63c65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6a22414e500d759df705",
+    "url": "/static/js/main.c83d1376.chunk.js"
+  },
+  {
+    "revision": "9bfffe6c1591ed4c2839",
+    "url": "/static/js/2.92421576.chunk.js"
+  },
+  {
+    "revision": "6a22414e500d759df705",
+    "url": "/static/css/main.f6ca7b31.chunk.css"
+  },
+  {
+    "revision": "1f82a7cdc7465d4381a91c70a36dd307",
+    "url": "/index.html"
+  }
+];
